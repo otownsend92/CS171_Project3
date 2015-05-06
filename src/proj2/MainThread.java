36289fb628408cfa181f62c353c7b6f65f5b9f4a
Oliver Townsend <otownsend92@gmail.com>
2015-05-05 19:09:35 -0700

    First commit

---
+++ src/proj2/MainThread.java
@@ -0,0 +1,15 @@
+package proj2;
+
+public class MainThread{
+	
+	private Thread[] eThread = new Thread[4];
+	private Thread[] sThread = new Thread[4];
+	
+	public MainThread(){
+		// Constructor
+	}
+	
+	public static void main(String[] args){
+		MainThread t = new MainThread();
+	}
+}
