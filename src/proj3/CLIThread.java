334121d8f8ad1108bf92e514241e24a5e7562210
Oliver Townsend <otownsend92@gmail.com>
2015-05-13 18:08:30 -0700

    doop

--- src/proj3/CLIThread.java
+++ src/proj3/CLIThread.java
@@ -4,40 +4,40 @@ import java.util.Scanner;
 
 public class CLIThread extends Thread {
 
-	
-	
 	public CLIThread() {
-		
+
 	}
-	
-	
+
 	public void run() {
 		ReadFromSystemIn();
 	}
-	
+
 	public void ReadFromSystemIn() {
 		Scanner sc = new Scanner(System.in);
-        System.out.println("Printing the file passed in:");
-        while(sc.hasNextLine()) {
-        	
-        	String input = sc.nextLine();
-//        	System.out.println("Parsing " + input);
-        	
-        	ParseInput(input);
-        }
-	}
-	
-	public void ParseInput(String input) {
-		if(input.substring(0, 1).equals("R")) {
-			input += " ";
+		System.out.println("Printing the file passed in:");
+		while (sc.hasNextLine()) {
+			String input = sc.nextLine();
+			ParseInput(input);
 		}
-		int len = input.length();
-		String command = input.substring(0,input.indexOf(' ')); 
-		String message = input.substring(input.indexOf(' ')+1); 
-		System.out.println("c,m: " + command + ", " + message + "|||");
-		
-		
-		
 	}
-	
+
+	public void ParseInput(String input) {
+		input += " ";
+
+		String command = input.substring(0, input.indexOf(' '));
+		String message = input.substring(input.indexOf(' ') + 1);
+
+		if (command.equals("Read")) {
+			System.out.println("Reading!!");
+		}
+		
+		else if (command.equals("Append")) {
+			if (message.length() > 0
+					&& message.charAt(message.length() - 1) == ' ') {
+				message = message.substring(0, message.length() - 1);
+			}
+			System.out.println("Appending " + message + "$");
+		}
+	}
+
 }
